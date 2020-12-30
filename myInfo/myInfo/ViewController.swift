170604e2374e39f7aaccdbe44fc2778b3cf932f9
sauravgehani <76703640+sauravgehani@users.noreply.github.com>
2020-12-30 16:35:15 -0500

    Add files via upload

---
+++ myInfo/myInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myInfo
+//
+//  Created by Shalini Gehani on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
